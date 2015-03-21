9c0eaf425e8fdad9c03128e53b01df436d74c167
Johnny Lee <jleeothon@gmail.com>
2015-03-21 17:54:14 -0600

    Updated core

--- core/extract.py
+++ core/extract.py
@@ -1,10 +1,12 @@
 # -*-coding:utf-8 -*-
 
+import os
 import sys
 import urllib2
 import sqlalchemy
+import settings
 
-TEXTS_FOLDER = 'texts/'
+TEXTS_FOLDER = os.path.join(settings.BASE_DIR, "texts")
 DEFAULT_TEXT = 'default.txt'
 EXTRA_CHARS = '",./\'-_?¿*:()[]{}¡!%$=0987654321“”‘’'
 
@@ -22,36 +24,34 @@ def flush_query(words):
 
 
 def get_text(url, query=True, author="Unknown", title="Unknown", period="Unknown"):
-    file_name = DEFAULT_TEXT if query else '_'.join([author, title]) + '.txt'
-    with open(TEXTS_FOLDER + file_name, 'wb') as text_file:
+    """
+    Gets the text from the url
+    """
+    filename = DEFAULT_TEXT if query else '-'.join([author, title]) + '.txt'
+    filename = os.path.join(TEXTS_FOLDER, filename)
+    with open(filename, 'wb') as text_file:
         response = urllib2.urlopen(url)
         text = response.read()
         text_file.write(text)
-    return file_name
+    return filename
 
 
 def read_text(file_name):
     words = {}
     sentences = {}
-
     with open(TEXTS_FOLDER + file_name, 'r') as text_file:
         line = text_file.readline()
         limit = 500
-        
         while line:
             line = line.split()
-
             for word in line:
                 word = word.strip(EXTRA_CHARS)
                 word = word.lower()
                 words[word] = words[word] + 1 if words.has_key(word) else 1
-            
             if limit == 0:
                 flush_query(words)
                 words = {}
                 limit = 500
-            
             limit -= 1
             line = text_file.readline()
-
         flush_query(words)
