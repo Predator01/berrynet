5dc176806bd893aff11ba939d9dfdae163f51904
Predator01 <miguel.agr92@gmail.com>
2015-03-21 22:54:18 -0600

    create database folder
    database creation in root

--- db/create.py
+++ db/create.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
+from core.settings import BASE_DIR
+import os
 
 db_name = 'database_mining.sqlite'
 db_prefix = 'sqlite:///'
-engine = create_engine('sqlite:///../database_mining.sqlite')
 
-
-Base.metadata.create_all(engine)
+def create_database():
+	path_folder = os.path.join(BASE_DIR,db_name)
+	path = db_prefix + path_folder
+	engine = create_engine(path)	
+	Base.metadata.create_all(engine)	
 
 def create_session():
 	Session = sessionmaker() 
